ac2d85236c9c4a7f071c6a8a0cea86df566bd5e1
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-02-03 19:55:42 +0600

    add router feature

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,8 +1,15 @@
+'use client'
+import { useRouter } from "next/navigation";
 
 const LoginPage = () => {
+    const router = useRouter();
+    const handleNavigation = () =>{
+        router.push('/dashboard')
+    }
     return (
         <div>
             <h1>Hello form log in page</h1>
+            <button onClick={handleNavigation}>Dashboard</button>
         </div>
     );
 };
--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,25 +1,26 @@
+import Link from "next/link";
 
 const Navbar = () => {
     return (
-        <nav className="bg-blue-600 text-white py-4 shadow-md">
+        <nav className="bg-blue-600 text-white py-4 shadow-md flex justify-between items-center">
             <div className="container mx-auto flex justify-between items-center">
-                <a href="/" className="text-2xl font-bold">
+                <Link href="/" className="text-2xl font-bold">
                     MySite
-                </a>
-            </div>
-            <div className="space-x-6">
-                <a href="/about" className="hover:text-gray-200">
+                </Link>
+            <div className="space-x-6 flex items-center gap-x-4">
+                <Link href="/about" className="hover:text-gray-200">
                     About us
-                </a>
-                <a href="/contact" className="hover:text-gray-200">
+                </Link>
+                <Link href="/contact" className="hover:text-gray-200">
                     Contact us
-                </a>
-                <a href="/login" className="hover:text-gray-200">
+                </Link>
+                <Link href="/login" className="hover:text-gray-200">
                     Login
-                </a>
-                <a href="/register" className="hover:text-gray-200">
+                </Link>
+                <Link href="/register" className="hover:text-gray-200">
                     Register
-                </a>
+                </Link>
+            </div>
             </div>
         </nav>
     );
