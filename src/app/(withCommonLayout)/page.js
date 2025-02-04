681a7390e93398060f0dc3477598232363c3fa0a
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-02-04 16:03:50 +0600

    add: data fetching and caching an revalidation

--- src/app/(withCommonLayout)/page.js
+++ src/app/(withCommonLayout)/page.js
@@ -1,11 +1,13 @@
 import ProductCard from "../../components/prodeucts/ProductCard.jsx"
 const HomePage = async() => {
-  const res = await fetch("http://localhost:5000/products");
+  const res = await fetch("http://localhost:5000/products",{
+    cache:"force-cache"
+  });
   const products = await res.json();
   return (
     <main>
       <h1>Data Fetching, Caching, and Revalidating </h1>
-      <div>
+      <div className="grid grid-cols-3 gap-4 container mx-auto">
         {
           products.map(product => <ProductCard key={product.id} product={product}/>)
         }
